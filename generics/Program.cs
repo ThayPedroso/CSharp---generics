fbaf2003c7ac4209c591cf60b4726f97e4fd1aad
Thayla Pedroso <thayla.pedroso88@gmail.com>
2021-02-08 21:36:30 -0300

    without generics, only for integers numbers

--- generics/Program.cs
+++ generics/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace generics
 {
@@ -6,7 +7,23 @@ namespace generics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> list = new List<int>();
+
+            Console.Write("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                list.Add(x);
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            int max = calculationService.Max(list);
+
+            Console.WriteLine("Max: ");
+            Console.WriteLine(max);
         }
     }
 }
